48923dc799389c5a1c2273d196da7fa80a5d70cd
LucasAtAeco <86406285+LucasAtAeco@users.noreply.github.com>
2025-05-12 16:59:57 +0200

    feat: add test data

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -6,7 +6,13 @@ export const GET = async (request: NextRequest) => {
 
   const calculation = 60 * 60 * 24 * 365;
 
-  return new NextResponse(`Hello, ${name}! Look at this: ${calculation}`, {
-    status: 200,
-  });
+  const test =
+    "Mollit velit eu quis non enim nulla quis elit eiusmod. Amet officia aute quis ex minim. Exercitation esse sint dolor ullamco anim. Aliquip sit officia sint reprehenderit et in reprehenderit pariatur consequat excepteur amet ad veniam est culpa. Veniam sunt tempor magna eiusmod occaecat. Nisi incididunt esse excepteur nisi. Quis tempor proident officia enim eiusmod dolore dolor ut veniam. Aliqua laboris ea consectetur duis Lorem et ut velit sunt eu do esse pariatur.";
+
+  return new NextResponse(
+    `Hello, ${name}! Look at this: ${calculation} or this ${test}`,
+    {
+      status: 200,
+    }
+  );
 };
